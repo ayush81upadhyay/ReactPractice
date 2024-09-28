b9e85cc7786ce1fedcce8b32343f83a3f07968b6
DEVD <ayush7upadhyay@gmail.com>
2024-09-28 23:52:29 +0530

    modified files

--- 02_JSX_Babel_Practice/script.js
+++ 02_JSX_Babel_Practice/script.js
@@ -1,4 +1,7 @@
-const h2 = <h1>Intro to JSX and Babel</h1>;
+const user = "Amitabh";
 
+// JSX alongwith JS variable & styling being used inside it within curly braces
+const h2 = (<h1 style={{color: "red"}}><p>Initials</p>Intro to JSX and Babel {user} </h1>);
+console.log(h2);
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(h2);
