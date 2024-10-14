62b6a633315f676d4443d91ca2d70415ca3a9bee
DEVD <ayush7upadhyay@gmail.com>
2024-10-14 12:39:46 +0530

    Final Render Multiple Code

---
+++ 06_Rendering_Multiple_Components_in_React/3. Rendering with Functional Component - Final code/index.js
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+import imgURL from './assests/images/apple-iphone-12-r1.jpg';
+import './style.css';
+
+// Created a function which is returning the jSX [key is required to be given]
+const Card = (key) => {
+    return (
+    <div className="card" key={key}>
+        <img src={imgURL} alt="iphone12"></img>
+        <h1>Apple</h1>
+        <h3>$24</h3>
+        <h3>5 Star</h3>
+        <h3>256 GB Ram</h3>
+    </div>
+    );
+}
+
+// Passed functions in an array with arguments in it
+const card_container = [Card(1), Card(2), Card(3), Card(4)];
+const root = createRoot(document.getElementById("root"));
+root.render(card_container);
